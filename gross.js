26c15c941eae012eef21c13fe7e2529335ea5427
Amol Srivastava <amosriva011@gmail.com>
2022-01-28 08:42:56 +0530

    Initial commit

---
+++ gross.js
@@ -0,0 +1,35 @@
+function incrementValue()
+{
+    var value=parseInt(document.getElementById('1').value,10);
+    value=isNaN(value) ? 0:value;
+    value++;
+    document.getElementById('1').value=value;
+}
+function incrementValu()
+{
+    var value=parseInt(document.getElementById('2').value,10);
+    value=isNaN(value) ? 0:value;
+    value++;
+    document.getElementById('2').value=value;
+}
+function incrementVal()
+{
+    var value=parseInt(document.getElementById('3').value,10);
+    value=isNaN(value) ? 0:value;
+    value++;
+    document.getElementById('3').value=value;
+}
+function incrementVa()
+{
+    var value=parseInt(document.getElementById('4').value,10);
+    value=isNaN(value) ? 0:value;
+    value++;
+    document.getElementById('4').value=value;
+}
+function incrementV()
+{
+    var value=parseInt(document.getElementById('5').value,10);
+    value=isNaN(value) ? 0:value;
+    value++;
+    document.getElementById('5').value=value;
+}
